437ad827bdab75c831f3cbc009c28119cad37b32
Guest User <student@epicodus.com>
2015-10-21 19:50:36 -0700

    add all of our tries for specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 var pigLatin = function(word) {
-  return false;
+  if (["a","e","i","o","u"].includes(word[0])); {
+    return (word.concat("ay"));
+  };
 };
+
+//var pigLatin = function(word) {
+//  if (word[0].indexOf([a-zA-Z]-["a","e","i","o","u"]))
+//  return (word);
+//};
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,10 @@
 //For words that start with a vowel, add "ay" to the end.
 describe('pigLatin', function() {
-  it("is false if it does not start with a vowel", function() {
-    expect(pigLatin("w")).to.equal(false);
+  it("adds ay to the word if it starts with a vowel", function() {
+    expect(pigLatin("apple")).to.equal("appleay");
   });
+
+  it("does not add ay to the word if it starts with a vowel"), function() {
+    expect(pigLatin("work")).to.equal("work");
+  }
 });
